3faa592a9dedee7be4dad2b9f2843a75a5a65cc9
gentlemanSoup <anders.horne@gmail.com>
2024-02-15 00:06:04 +0100

    fdh

--- Runtime/ActorController.cs
+++ Runtime/ActorController.cs
@@ -6,11 +6,13 @@ namespace Pixygon.Actors {
         public virtual void Sleep() {
             //if(_rigid != null) _rigid.Sleep();
             //if(_isAnimNotNull) _anim.speed = 0f;
+            //if(_isAnimNotNull && _anim != null) _anim.speed = 0f;
         }
 
         public virtual void WakeUp() {
             //if(_rigid != null) _rigid.WakeUp();
             //if(_isAnimNotNull) _anim.speed = _defaultAnimSpeed;
+            //if(_isAnimNotNull && _anim != null) _anim.speed = _defaultAnimSpeed;
         }
     }
 }
