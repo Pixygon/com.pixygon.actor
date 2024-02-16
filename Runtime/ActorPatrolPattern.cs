72bb3690877b82b48697302c4b0f81074cc349f2
gentlemanSoup <anders.horne@gmail.com>
2024-02-16 11:17:49 +0100

    arsedtrfyguih

--- Runtime/ActorPatrolPattern.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Pixygon.Actors {
-    public class ActorPatrolPattern : MonoBehaviour {
-        [SerializeField] private PatrolData _patrolData;
-        public PatrolData PatrolData => _patrolData;
-    }
-}
