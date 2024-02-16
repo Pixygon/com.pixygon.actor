0272d5fc6ce35fb16f5798c68fea1000e3ab1860
gentlemanSoup <anders.horne@gmail.com>
2024-02-16 11:14:29 +0100

    Update PatrolData.cs

--- Runtime/PatrolData.cs
+++ Runtime/PatrolData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace Pixygon.Actors {
     [Serializable]
     public class PatrolData {
+        public bool _usePatrol;
         public float _patrolGoalDistance = .5f;
         public float _patrolSpeed = .2f;
         public float _maxDegreesDelta = 600f;
