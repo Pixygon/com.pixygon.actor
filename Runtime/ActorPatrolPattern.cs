b7eae2c1e81616e51eb8a85ff3ae3222064f6688
gentlemanSoup <anders.horne@gmail.com>
2024-02-16 11:06:14 +0100

    Update ActorPatrolPattern.cs

--- Runtime/ActorPatrolPattern.cs
+++ Runtime/ActorPatrolPattern.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 
 namespace Pixygon.Actors {
     public class ActorPatrolPattern : MonoBehaviour {
-        [SerializeField] private Vector3[] _positions;
         [SerializeField] private PatrolData _patrolData;
-        public Vector3[] Positions => _positions;
         public PatrolData PatrolData => _patrolData;
     }
 }
