86362fb5a46f518a7beea819fee306a3d14f379a
gentlemanSoup <anders.horne@gmail.com>
2024-03-18 14:53:16 +0100

    gfdn

--- Runtime/PatrolData.cs
+++ Runtime/PatrolData.cs
@@ -1,29 +1,11 @@
 ﻿using System;
-using UnityEngine;
 
 namespace Pixygon.Actors {
     [Serializable]
     public class PatrolData {
         public bool _usePatrol;
-        public float _patrolGoalDistance = .5f;
-        public float _patrolSpeed = .2f;
+        public float _patrolGoalDistance = .2f;
         public float _maxDegreesDelta = 600f;
-        public bool _patrolOnlyLook;
-        public float _patrolWaitTime;
-        public Vector3[] _patrolPoints;
         public PatrolPointData[] _patrolPointDatas;
-        
-        public void UpgradeToNewSystem() {
-            _patrolPointDatas = new PatrolPointData[_patrolPoints.Length];
-            for (var i = 0; i < _patrolPoints.Length; i++) {
-                _patrolPointDatas[i] = new PatrolPointData() {
-                    _pos = _patrolPoints[i],
-                    _speed = _patrolSpeed,
-                    _onlyLook = _patrolOnlyLook,
-                    _useLocalPos = false,
-                    _waitTime = _patrolWaitTime
-                };
-            }
-        }
     }
 }
--- Runtime/PatrolPointData.cs
+++ Runtime/PatrolPointData.cs
@@ -9,5 +9,13 @@ namespace Pixygon.Actors {
         public bool _onlyLook;
         public float _speed = .2f;
         public float _waitTime = .5f;
+
+        public PatrolPointData() {
+            _useLocalPos = true;
+            _pos = Vector3.zero;
+            _onlyLook = false;
+            _speed = .2f;
+            _waitTime = .5f;
+        }
     }
 }
