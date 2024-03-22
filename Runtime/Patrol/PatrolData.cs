e438ed1d749da8ae182f8f52bf19f5ae8a030ee3
gentlemanSoup <anders.horne@gmail.com>
2024-03-22 18:17:46 +0100

    Added PatrolPatterns!

---
+++ Runtime/Patrol/PatrolData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pixygon.Actors {
+    [Serializable]
+    public class PatrolData {
+        public bool _usePatrol;
+        public float _patrolGoalDistance = .2f;
+        public float _maxDegreesDelta = 600f;
+        [SerializeField] private PatrolPattern _patrolPattern;
+        [SerializeField] private PatrolPointData[] _patrolPointDatas;
+
+        public PatrolPointData[] PatrolPointData =>
+            _patrolPattern == null ? _patrolPointDatas : _patrolPattern._patrolPointDatas;
+    }
+}
---
+++ Runtime/Patrol/PatrolPattern.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Pixygon.Actors {
+    [CreateAssetMenu(menuName = "Pixygon/Actors/New PatrolPattern", fileName = "New PatrolPattern", order = 0)]
+    public class PatrolPattern : ScriptableObject {
+        public PatrolPointData[] _patrolPointDatas;
+    }
+}
