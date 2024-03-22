093a73cc41579b6e581d45a193bf092116350f7d
gentlemanSoup <anders.horne@gmail.com>
2024-03-22 14:33:16 +0100

    Update PatrolPointData.cs

--- Runtime/PatrolPointData.cs
+++ Runtime/PatrolPointData.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Pixygon.Actors {
     [Serializable]
     public class PatrolPointData {
-        public bool _useLocalPos;
+        [FormerlySerializedAs("_useLocalPos")] public bool _useWorldPos;
         public Vector3 _pos;
         public bool _onlyLook;
         public float _speed = .2f;
         public float _waitTime = .5f;
 
         public PatrolPointData() {
-            _useLocalPos = true;
+            _useWorldPos = true;
             _pos = Vector3.zero;
             _onlyLook = false;
             _speed = .2f;
